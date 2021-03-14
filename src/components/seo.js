3e197ddb3ee2fd8fc0fa1b37c609d11e31d9c499
xxhyxayxx <xxhyxayxx0619@gmail.com>
2021-03-14 09:55:16 +0900

    :bug: add lang

--- src/components/seo.js
+++ src/components/seo.js
@@ -27,7 +27,9 @@ const SEO = ({ title, description, image, article }) => {
     url: `${siteUrl}${pathname}`,
   }
   return (
-      <Helmet title={seo.title}>
+      <Helmet title={seo.title} htmlAttributes={{
+        lang: 'ja',
+      }}>
         <meta name="description" content={seo.description} />
         <meta name="image" content={seo.image} />
         {seo.url && <meta property="og:url" content={seo.url} />}
